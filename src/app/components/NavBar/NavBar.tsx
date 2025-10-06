621d6722884bb0b37800d2a1ace0cc4b86289e3f
yugalp2000 <yugalp2000@gmail.com>
2025-10-06 10:02:00 +0530

    Updated navbar

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -11,7 +11,7 @@ const NavBar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
                     </div>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li><Link href='/home'>Homepage</Link></li>
+                        {/* <li><Link href='/home'>Homepage</Link></li> */}
                         <li><Link href='/projects'>Projects</Link></li>
                         <li><Link href='/about-us'>About</Link></li>
                     </ul>
